a680fe66a28bd25cbbb9c31a6c980504e8717e55
Manoj <manojsinghrana467@gmail.com>
2021-09-17 04:57:49 -0700

    Sum of two Value

--- new.sh
+++ new.sh
@@ -1,6 +1,8 @@
 #!bin/bash
-function first(){
-echo $1
-}
+echo -n "Enter first Number:"
+read num1
+echo -n "Enter Second  Number:"
+read num2
+sum=`expr $num1 + $num2`
+echo "Sum of two value is $sum"
 
-first lpu
